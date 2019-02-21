b34f68eeba4ac73d150c12ee6106569fc3a4d035
I-biubiubiu <273998074@qq.com>
2019-02-21 17:26:10 +0800

    审查,权限1

--- app/AdminPermission.php
+++ app/AdminPermission.php
@@ -12,4 +12,5 @@ class AdminPermission extends Model
     public function roles() {
         return $this->belongsToMany(\App\AdminRole::class, 'admin_permission_role', 'permission_id', 'role_id')->withPivot(['permission_id', 'role_id']);
     }
+    
 }
